57d228f0bc98ff4b4c7d5a3634e0aeed93ce8472
jerred-shifflett <jerredshifflett@gmail.com>
2021-05-31 17:06:55 -0700

    wrote second test, leapyear=false

--- Leapyear/test_leapyear.py
+++ Leapyear/test_leapyear.py
@@ -5,6 +5,8 @@ class testCase(unittest.TestCase):
 	
 	def test1(self):
 		self.assertEqual(leapyear.isItLeapYear(2000),True)
+	def test2(self):
+		self.assertEqual(leapyear.isItLeapYear(2019),False)
 	
 if __name__ == '__main__':
 	unittest.main()
