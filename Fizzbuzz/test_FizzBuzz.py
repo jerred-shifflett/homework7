5f7db865316d988badea54d60e6a926be7158f99
jerred-shifflett <jerredshifflett@gmail.com>
2021-05-31 17:21:10 -0700

    wrote test for Fuzz results and Bizz results

--- Fizzbuzz/test_FizzBuzz.py
+++ Fizzbuzz/test_FizzBuzz.py
@@ -5,6 +5,10 @@ class testCase(unittest.TestCase):
 	
 	def test1(self):
 		self.assertEqual(FizzBuzz.fizzBuzz(15),"FizzBuzz")
+	def test2(self):
+		self.assertEqual(FizzBuzz.fizzBuzz(9),"Fizz")
+	def test3(self):
+		self.assertEqual(FizzBuzz.fizzBuzz(10),"Buzz")
 	
 if __name__ == '__main__':
 	unittest.main()
